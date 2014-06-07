89f0c4a984f8b3342c4b27a873e41e7f3774ea34
TehKitti <mctehkitti@gmail.com>
2014-06-07 12:20:16 -0400

    Begin code reworking
    beginning the reworking of maunz's code, starting with main.java

--- src/me/TehKitti/Maunz/Main.java
+++ src/me/TehKitti/Maunz/Main.java
@@ -1,20 +1,16 @@
 package me.TehKitti.Maunz;
 
-import java.io.IOException;
-
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
-import org.pircbotx.exception.IrcException;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class Main {
 	public static PircBotX bot;
 	static boolean isEnabled = true;
 
-	public static void main(String args[]) {
-		Configuration config = new Configuration.Builder().setName("Maunz")
+		static Configuration config = new Configuration.Builder().setName("Maunz")
 				.setVersion("1.0")
-				.setServerHostname("chaos.esper.net")
+				.setServerHostname("irc.esper.net")
 				.setServerPort(6667)
 				.setLogin("Maunz")
 				.addAutoJoinChannel("#bl4ckscor3")
@@ -35,59 +31,16 @@ public class Main {
 				.setRealName("Maunz, an IRC bot coded by TehKitti.")
 				.buildConfiguration();
 
+	public static void main(String args[]) throws Exception {
 		bot = new PircBotX(config);
-		
-		if(!bot.isConnected()) {
-			while(true) {
-				try {
-					bot.startBot();
-				}
-				catch(Exception e) {
-				}
-			}
-		}
-		
+		bot.startBot();
 		isEnabled = true;
 
 	}
 
-	public static void main2() {
-		Configuration config = new Configuration.Builder()
-		        .setName("Maunz")
-				.setVersion("1.0")
-				.setServerHostname("chaos.esper.net")
-				.setServerPort(6667)
-				.setLogin("Maunz")
-				.addAutoJoinChannel("#bl4ckscor3")
-				.setNickservPassword("HIDDEN")
-				.setAutoNickChange(true)
-				.setCapEnabled(true)
-				.addListener(new Stop())
-				.addListener(new Restart())
-				.addListener(new Ping())
-				.addListener(new Help())
-				.addListener(new Say())
-				.addListener(new Other())
-				.addListener(new EnableAndDisable())
-				.addListener(new AccountInfo())
-				.addListener(new Source())
-				//.addListener(new UUID())
-				.setMessageDelay(400)
-				.setRealName("Maunz, an IRC bot coded by TehKitti.")
-				.buildConfiguration();
-
+	public static void main2() throws Exception {
 		bot = new PircBotX(config);
-				
-		if(!bot.isConnected()) {
-			while(true) {
-				try {
-					bot.startBot();
-				}
-				catch(Exception e) {
-				}
-			}
-		}
-		
+		bot.startBot();
 		isEnabled = true;
 
 	}
