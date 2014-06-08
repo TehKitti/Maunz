59857e5dfc088536ae9711382286882da097fb9a
TehKitti <mctehkitti@gmail.com>
2014-06-08 19:43:20 -0400

    Move help documents to website
    With the growing amount of commands its getting more annoying for the
    help messages to come through so I've moved them to my site.

--- src/me/TehKitti/Maunz/Commands/Help.java
+++ src/me/TehKitti/Maunz/Commands/Help.java
@@ -2,7 +2,6 @@ package me.TehKitti.Maunz.Commands;
 
 import me.TehKitti.Maunz.Core.Main;
 
-import org.pircbotx.Colors;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PrivateMessageEvent;
@@ -14,78 +13,7 @@ public class Help extends ListenerAdapter {
 	public void onMessage(MessageEvent event) throws Exception {
 		if (Main.isEnabled == true) {
 			if (event.getMessage().equalsIgnoreCase("*help")) {
-				event.getUser()
-						.send()
-						.notice(Colors.TEAL
-								+ "------------------ Maunz Help ------------------");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "All Maunz help documents are located in this command. Find the thing you need help with and read its description and syntax!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "All commands listed here can also be done via PM/query to Maunz.");
-				event.getUser()
-						.send()
-						.notice(Colors.TEAL
-								+ "---------------- Maunz Commands ----------------");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*stop   Stops Maunz. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*restart   Restarts Maunz. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*enable   Enables Maunz if she has been disabled with *disable. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*disable   Disables Maunz. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*ping   Makes Maunz respond to you with Pong. Very useful for testing ping to the IRC server.");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*say <message>   Makes Maunz say whatever you want her to!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*accinfo <account>   Tells you whether or not a Minecraft account is premium!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*source   Links you to the GitHub page of Maunz, you can submit issues/pull requests here.");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*help   Opens what you're looking at right now!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*uuid <username>   Will return the Minecraft UUID of the username you requested.");
-				event.getUser()
-						.send()
-						.notice(Colors.TEAL
-								+ "----------------- Maunz Credits ----------------");
-				event.getUser().send()
-						.notice(Colors.MAGENTA + "Created By TehKitti.");
-				event.getUser().send()
-						.notice(Colors.MAGENTA + "Help From bl4ckscor3.");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "Premium account status grabbed from http://axis.iaero.me");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "Coded in Java 7 using PircBotX 2.0.1 and its dependencies: https://code.google.com/p/pircbotx/");
+				event.respond("Help documents are located at http://tehkitti.pw/maunz.html");
 			}
 		}
 	}
@@ -93,78 +21,7 @@ public class Help extends ListenerAdapter {
 	public void onPrivateMessage(PrivateMessageEvent event) throws Exception {
 		if (Main.isEnabled == true) {
 			if (event.getMessage().equalsIgnoreCase("*help")) {
-				event.getUser()
-						.send()
-						.notice(Colors.TEAL
-								+ "------------------ Maunz Help ------------------");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "All Maunz help documents are located in this command. Find the thing you need help with and read its description and syntax!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "All commands listed here can also be done via PM/query to Maunz.");
-				event.getUser()
-						.send()
-						.notice(Colors.TEAL
-								+ "---------------- Maunz Commands ----------------");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*stop   Stops Maunz. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*restart   Restarts Maunz. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*enable   Enables Maunz if she has been disabled with *disable. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*disable   Disables Maunz. Only useable by TehKitti!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*ping   Makes Maunz respond to you with Pong. Very useful for testing ping to the IRC server.");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*say <message>   Makes Maunz say whatever you want her to!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*accinfo <account>   Tells you whether or not a Minecraft account is premium!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*source   Links you to the GitHub page of Maunz, you can submit issues/pull requests here.");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*help   Opens what you're looking at right now!");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "*uuid <username>   Will return the Minecraft UUID of the username you requested.");
-				event.getUser()
-						.send()
-						.notice(Colors.TEAL
-								+ "----------------- Maunz Credits ----------------");
-				event.getUser().send()
-						.notice(Colors.MAGENTA + "Created By TehKitti.");
-				event.getUser().send()
-						.notice(Colors.MAGENTA + "Help From bl4ckscor3.");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "Premium account status grabbed from http://axis.iaero.me");
-				event.getUser()
-						.send()
-						.notice(Colors.MAGENTA
-								+ "Coded in Java 7 using PircBotX 2.0.1 and its dependencies: https://code.google.com/p/pircbotx/");
+				event.respond("Help documents are located at http://tehkitti.pw/maunz.html");
 			}
 		}
 	}
