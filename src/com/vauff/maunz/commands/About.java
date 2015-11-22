b99dd066d18f651516c10c1eaaea4e6ea624e9f5
Vauff <mctehkitti@gmail.com>
2015-11-22 13:48:44 -0500

    Fix build date - Fixed build date

--- src/com/vauff/maunz/commands/About.java
+++ src/com/vauff/maunz/commands/About.java
@@ -11,7 +11,7 @@ import com.vauff.maunz.core.Util;
 
 public class About implements ICommand<MessageEvent<PircBotX>, PrivateMessageEvent<PircBotX>>
 {
-	String builddate = "Saturday November 22nd 2015, 4:11 PM GMT";
+	String builddate = "Sunday November 22nd 2015, 6:46 PM GMT";
 
 	@Override
 	public void exeChan(MessageEvent<PircBotX> event) throws Exception
