180c4ad09c93a5d03d830f53c93a5a1cdc196e90
Vauff <mctehkitti@gmail.com>
2016-08-09 17:49:16 -0400

    Attempted fix for GFL ZE tracking frequently timing out
    - Attempted a fix to stop the GFL ZE map tracking from timing out so much
    - Added error for map tracking timeout

--- src/com/vauff/maunz/features/GFLTimer.java
+++ src/com/vauff/maunz/features/GFLTimer.java
@@ -1,5 +1,7 @@
 package com.vauff.maunz.features;
 
+import java.net.SocketTimeoutException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.pircbotx.Colors;
@@ -17,7 +19,7 @@ public class GFLTimer
 		{
 			try
 			{
-				Document doc = Jsoup.connect("https://stats.gflclan.com/hlstats.php?game=csgoze").get();
+				Document doc = Jsoup.connect("https://stats.gflclan.com/hlstats.php?game=csgoze").timeout(10000).get();
 				String html = doc.select("td[class=game-table-cell]").text();
 				String[] mapSplit = html.split(" ");
 				String map = "";
@@ -36,6 +38,10 @@ public class GFLTimer
 					Util.msg(Util.privateChannel, "GFL Zombie Escape is now playing: " + Colors.MAGENTA + Colors.BOLD + map);
 					lastMap = map;
 				}
+			}
+			catch (SocketTimeoutException e)
+			{
+				Logger.log.error("Failed to connect to the GFL HLStatsX page, automatically retrying in 1 minute");
 			}
 			catch (Exception e)
 			{
